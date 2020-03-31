3cb5c7b9bcd5915298aa13a24e4671917f7c3c67
MaharshiYeluri01 <40265954+MaharshiYeluri01@users.noreply.github.com>
2020-03-31 16:36:52 +0530

    custom superscript detection

--- pptx/text/text.py
+++ pptx/text/text.py
@@ -338,7 +338,32 @@ class Font(object):
     @italic.setter
     def italic(self, value):
         self._rPr.i = value
+        
+    @property
+    def superscript(self):
+        # baseline of 30000 means superscript
+        if self._rPr.baseline == 30000:
+            return True,self._rPr.baseline
+        return False,self._rPr.baseline
 
+    @superscript.setter
+    def superscript(self, value):
+        self._rPr.baseline = 0
+        if value:
+            self._rPr.baseline = 30000
+
+    @property
+    def subscript(self):
+        # baseline of -25000 means subscript
+        if self._rPr.baseline == -25000:
+            return True
+        return False
+
+    @subscript.setter
+    def subscript(self, value):
+        self._rPr.baseline = 0
+        if value:
+            self._rPr.baseline = -25000
     @property
     def language_id(self):
         """
